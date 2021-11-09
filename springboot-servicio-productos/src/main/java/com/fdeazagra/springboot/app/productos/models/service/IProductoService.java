02a0efa2a9cbd94572bb58118d38fa5bd54ce045
Fernando Azagra <73386163+fdeazagra@users.noreply.github.com>
2021-11-09 19:57:01 +0100

    Se añade Dao y Service

---
+++ springboot-servicio-productos/src/main/java/com/fdeazagra/springboot/app/productos/models/service/IProductoService.java
@@ -0,0 +1,11 @@
+package com.fdeazagra.springboot.app.productos.models.service;
+
+import java.util.List;
+
+import com.fdeazagra.springboot.app.productos.models.entity.Producto;
+
+public interface IProductoService {
+
+	public List<Producto> findAll();
+	public Producto findById(Long id);
+}
